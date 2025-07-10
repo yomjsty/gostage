1310603656b0bf5f2385cd3f5492d5b6c26b218b
yomjsty <oweychanges@gmail.com>
2025-07-11 03:14:04 +0700

    loading indicate progress bar

--- app/react-query-provider.tsx
+++ app/react-query-provider.tsx
@@ -2,8 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactNode, useState } from 'react'
+import { AppProgressProvider as ProgressProvider } from '@bprogress/next';
 
 export function ReactQueryProvider({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient())
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    return <QueryClientProvider client={queryClient}>
+        <ProgressProvider
+            height="3px"
+            color="#3b82f6"
+            options={{ showSpinner: true }}
+            shallowRouting
+        >
+            {children}
+        </ProgressProvider>
+    </QueryClientProvider>
 }
