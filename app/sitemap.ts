927ecac3917c60c28e77b310308a4fea7e685577
yomjsty <oweychanges@gmail.com>
2025-07-15 01:25:24 +0700

    fix sitemap couldnt be fetched

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 import db from "@/lib/db";
 import { env } from "@/lib/env";
 import { MetadataRoute } from "next";
-
-export const dynamic = 'force-dynamic';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = env.BETTER_AUTH_URL || 'https://gostage.vercel.app';
@@ -11,38 +11,26 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
         {
             url: baseUrl,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'daily' as const,
-            priority: 1,
         },
         {
             url: `${baseUrl}/about`,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'monthly' as const,
-            priority: 0.5,
         },
         {
             url: `${baseUrl}/contact`,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'monthly' as const,
-            priority: 0.5,
         },
         {
             url: `${baseUrl}/faq`,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'monthly' as const,
-            priority: 0.5,
         },
         {
             url: `${baseUrl}/privacy`,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'monthly' as const,
-            priority: 0.5,
         },
         {
             url: `${baseUrl}/terms`,
             lastModified: new Date().toISOString(),
-            changeFrequency: 'monthly' as const,
-            priority: 0.5,
         },
     ];
 
@@ -50,9 +38,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const eventRoutes = events.map((event) => ({
         url: `${baseUrl}/event/${event.slug}`,
         lastModified: event.updatedAt.toISOString(),
-        changeFrequency: 'weekly' as const,
-        priority: 0.7,
-        images: [`https://${env.NEXT_PUBLIC_UPLOADTHING_APP_ID}.ufs.sh/f/${event.featuredImage}`],
     }));
 
     return [...staticRoutes, ...eventRoutes];
@@ -64,7 +49,6 @@ async function getEvents() {
             status: "PUBLISHED",
         },
         select: {
-            featuredImage: true,
             slug: true,
             updatedAt: true,
         }
